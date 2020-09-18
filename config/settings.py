ea7a9f2b50d1ecfaed805639b15f0eb87ea14214
arthur_wxy <kzzn@qq.com>
2020-09-18 13:36:14 +0800

    .

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""=================================================
+@Project -> File   ：ApiTest -> settings
+@IDE    ：PyCharm
+@Author ：ArthurWxy
+@Date   ：2020/9/18 11:22 AM
+@Desc   ：
+=================================================="""
+
---
+++ config/test_data.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""=================================================
+@Project -> File   ：ApiTest -> test_data
+@IDE    ：PyCharm
+@Author ：ArthurWxy
+@Date   ：2020/9/18 11:29 AM
+@Desc   ：
+=================================================="""
